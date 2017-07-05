16d8798b461cb6e8e3297a06bf5f5fe2e9b11337
stephengrider <ste.grider@gmail.com>
2017-07-05 16:33:24 -0700

    followup deploy

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
+  res.send({ bye: 'buddy' });
 });
 
 const PORT = process.env.PORT || 5000;
