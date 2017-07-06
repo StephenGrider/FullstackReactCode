d1e6cb9b1585761e991f79828335d8fc0cdc19d7
stephengrider <ste.grider@gmail.com>
2017-07-06 11:13:51 -0700

    google strat

--- server/index.js
+++ server/index.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
 
-passport.use(new GoogleStrategy());
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
