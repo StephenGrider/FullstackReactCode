ca1513e2a4206ee0774783f9f53a381ec30a707a
stephengrider <ste.grider@gmail.com>
2017-07-05 17:34:40 -0700

    installed passport

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ bye: 'buddy' });
-});
+passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
