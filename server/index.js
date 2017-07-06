ad94e242dca59afe3b8c9b374fe6e4612d90cb45
stephengrider <ste.grider@gmail.com>
2017-07-05 17:54:38 -0700

    generated google keys

--- server/index.js
+++ server/index.js
@@ -4,6 +4,9 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
 
+// https://console.developers.google.com
+// client id: 1078055607998-7guji31g81t4mpe2bmpr981fftt9l4pu.apps.googleusercontent.com
+// client secret: VWHkdS6I0jKfzSjEX_EoChok
 passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000;
