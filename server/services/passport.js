63bfd0fa3ba036165236f58a7bee7417aa83d1ca
stephengrider <ste.grider@gmail.com>
2017-07-07 12:22:54 -0700

    serialize user

--- server/services/passport.js
+++ server/services/passport.js
@@ -5,6 +5,10 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
 passport.use(
   new GoogleStrategy(
     {
