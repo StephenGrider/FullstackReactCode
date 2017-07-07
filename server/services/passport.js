cc02521b9ddf91e74e9087b15a19b22b50efa201
stephengrider <ste.grider@gmail.com>
2017-07-07 12:27:50 -0700

    deserialize user

--- server/services/passport.js
+++ server/services/passport.js
@@ -9,6 +9,12 @@ passport.serializeUser((user, done) => {
   done(null, user.id);
 });
 
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
 passport.use(
   new GoogleStrategy(
     {
