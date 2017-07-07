d8c740040d80f92c7ea2d623ece079df081a2ca5
stephengrider <ste.grider@gmail.com>
2017-07-07 10:47:51 -0700

    save model instances

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
-require('./services/passport');
 require('./models/User');
+require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
 
--- server/services/passport.js
+++ server/services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
 
 passport.use(
   new GoogleStrategy(
@@ -10,9 +13,7 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log('access token', accessToken);
-      console.log('refresh token', refreshToken);
-      console.log('profile', profile);
+      new User({ googleId: profile.id }).save();
     }
   )
 );
