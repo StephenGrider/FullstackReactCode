f03e04d4dab1808fbb9c1908c77c3cb4f096feaf
stephengrider <ste.grider@gmail.com>
2017-07-07 12:52:15 -0700

    auth handler

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -9,4 +9,8 @@ module.exports = app => {
   );
 
   app.get('/auth/google/callback', passport.authenticate('google'));
+
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user);
+  });
 };
