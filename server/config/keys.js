bd7faf2606b88eee413b6013749c8796ccd43e62
stephengrider <ste.grider@gmail.com>
2017-07-07 15:46:34 -0700

    separate keys

---
+++ server/config/keys.js
@@ -0,0 +1,8 @@
+// keys.js - figure out what set of credentials to return
+if (process.env.NODE_ENV === 'production') {
+  // we are in production - return the prod set of keys
+  module.exports = require('./prod');
+} else {
+  // we are in development - return the dev keys!!!
+  module.exports = require('./dev');
+}
---
+++ server/config/prod.js
@@ -0,0 +1,7 @@
+// prod.js - production keys here!!
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
+};
