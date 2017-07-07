066b6d7a46e13adaca1a3dd072803289cec94828
stephengrider <ste.grider@gmail.com>
2017-07-07 12:58:38 -0700

    log users out

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -10,6 +10,11 @@ module.exports = app => {
 
   app.get('/auth/google/callback', passport.authenticate('google'));
 
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
+
   app.get('/api/current_user', (req, res) => {
     res.send(req.user);
   });
