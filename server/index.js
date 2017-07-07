48471405577e908596af2d910369da6355a53c70
stephengrider <ste.grider@gmail.com>
2017-07-06 17:37:10 -0700

    connect mongoose to mongo

--- server/index.js
+++ server/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
