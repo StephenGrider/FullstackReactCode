39b39da7a238b67b165ba51491a163aaafef83b5
stephengrider <ste.grider@gmail.com>
2017-07-07 16:28:24 -0700

    fixed proxy issue

--- server/services/passport.js
+++ server/services/passport.js
@@ -20,7 +20,8 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
+      callbackURL: '/auth/google/callback',
+      proxy: true
     },
     (accessToken, refreshToken, profile, done) => {
       User.findOne({ googleId: profile.id }).then(existingUser => {
