8fd6e236088ae5df26c14478886e5c5b5e070aa0
stephengrider <ste.grider@gmail.com>
2017-07-07 18:07:12 -0700

    stumble on routing

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <a href="/auth/google">Sign In With Google</a>
       </div>
     );
   }
