0b0fd9b648418f2fab8e0af2581f9fc37b0cc166
stephengrider <ste.grider@gmail.com>
2017-07-10 14:39:05 -0700

    Added more route config

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -11,7 +11,8 @@ const App = () => {
     <div>
       <BrowserRouter>
         <div>
-          <Route path="/" component={Landing} />
+          <Route exact path="/" component={Landing} />
+          <Route path="/surveys" component={Dashboard} />
         </div>
       </BrowserRouter>
     </div>
