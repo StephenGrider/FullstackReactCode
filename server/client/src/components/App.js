5c4f7ccebb12f0cbcecddb043b4ca536ddd7cd9a
stephengrider <ste.grider@gmail.com>
2017-07-10 14:23:59 -0700

    setup with react router

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
   return (
