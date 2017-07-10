94519cdec6268b16c540a046aecc1c86b0c036ac
stephengrider <ste.grider@gmail.com>
2017-07-10 12:43:21 -0700

    react setup

---
+++ server/client/src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      Hi There!
+    </div>
+  );
+};
+
+export default App;
--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render();
+import App from './components/App';
+
+ReactDOM.render(<App />, document.querySelector('#root'));
