b25efc23888327811b1151dc077f1f3699a8c021
stephengrider <ste.grider@gmail.com>
2017-07-10 14:45:48 -0700

    addedh eader

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -11,8 +11,10 @@ const App = () => {
     <div>
       <BrowserRouter>
         <div>
+          <Header />
           <Route exact path="/" component={Landing} />
-          <Route path="/surveys" component={Dashboard} />
+          <Route exact path="/surveys" component={Dashboard} />
+          <Route path="/surveys/new" component={SurveyNew} />
         </div>
       </BrowserRouter>
     </div>
