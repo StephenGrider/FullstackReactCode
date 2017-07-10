507484aeeb38c04488a78604fc7f3875c47daad4
stephengrider <ste.grider@gmail.com>
2017-07-10 16:06:30 -0700

    header design

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -3,9 +3,18 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
-      <div>
-        Header
-      </div>
+      <nav>
+        <div className="nav-wrapper">
+          <a className="left brand-logo">
+            Emaily
+          </a>
+          <ul className="right">
+            <li>
+              <a>Login With Google</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     );
   }
 }
