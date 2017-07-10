0e756d9c349b33be953c18defca9687c4b3de237
stephengrider <ste.grider@gmail.com>
2017-07-10 15:50:36 -0700

    Added materialize

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-const Header = () => <h2>Header</h2>;
+import Header from './Header';
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
 const Landing = () => <h2>Landing</h2>;
---
+++ server/client/src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        Header
+      </div>
+    );
+  }
+}
+
+export default Header;
