4eb733855357effc7ebf598143134d927e4777ee
stephengrider <ste.grider@gmail.com>
2017-07-11 11:58:56 -0700

    redirect on logout

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -9,7 +9,7 @@ class Header extends Component {
       case false:
         return <li><a href="/auth/google">Login With Google</a></li>;
       default:
-        return <li><a>Logout</a></li>;
+        return <li><a href="/api/logout">Logout</a></li>;
     }
   }
 
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -18,7 +18,7 @@ module.exports = app => {
 
   app.get('/api/logout', (req, res) => {
     req.logout();
-    res.send(req.user);
+    res.redirect('/');
   });
 
   app.get('/api/current_user', (req, res) => {
