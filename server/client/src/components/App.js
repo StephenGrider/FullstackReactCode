b221586afe64c99c68cdc303dd2e8dc12e0242a4
stephengrider <ste.grider@gmail.com>
2017-07-11 12:35:55 -0700

    landing component

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -4,9 +4,9 @@ import { connect } from 'react-redux';
 import * as actions from '../actions';
 
 import Header from './Header';
+import Landing from './Landing';
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
-const Landing = () => <h2>Landing</h2>;
 
 class App extends Component {
   componentDidMount() {
---
+++ server/client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>
+        Emaily!
+      </h1>
+      Collect feedback form your users
+    </div>
+  );
+};
+
+export default Landing;
