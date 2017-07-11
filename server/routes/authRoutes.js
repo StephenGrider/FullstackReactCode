47afab3455b76d464a827980feafd1898c2c75ea
stephengrider <ste.grider@gmail.com>
2017-07-11 11:46:41 -0700

    redirect

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -8,7 +8,13 @@ module.exports = app => {
     })
   );
 
-  app.get('/auth/google/callback', passport.authenticate('google'));
+  app.get(
+    '/auth/google/callback',
+    passport.authenticate('google'),
+    (req, res) => {
+      res.redirect('/surveys');
+    }
+  );
 
   app.get('/api/logout', (req, res) => {
     req.logout();
