c9ba220069801599b09597d7b007667418729edc
stephengrider <ste.grider@gmail.com>
2017-07-10 17:20:15 -0700

    basics of redux thunk

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -2,5 +2,9 @@ import axios from 'axios';
 import { FETCH_USER } from './types';
 
 const fetchUser = () => {
-  axios.get('/api/current_user');
+  return function(dispatch) {
+    axios
+      .get('/api/current_user')
+      .then(res => dispatch({ type: FETCH_USER, payload: res }));
+  };
 };
