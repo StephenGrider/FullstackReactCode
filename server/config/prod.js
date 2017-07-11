97eafdbbe444e7963a0b131fc8201f0c9dcef486
stephengrider <ste.grider@gmail.com>
2017-07-11 15:48:52 -0700

    added keys

--- server/config/prod.js
+++ server/config/prod.js
@@ -3,5 +3,7 @@ module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
-  cookieKey: process.env.COOKIE_KEY
+  cookieKey: process.env.COOKIE_KEY,
+  stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY
 };
