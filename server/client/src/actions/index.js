45f8bd55a4f92ed926ef39547b8d05942a0ec425
stephengrider <ste.grider@gmail.com>
2017-07-11 11:11:17 -0700

    auth reducer return value

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -4,5 +4,5 @@ import { FETCH_USER } from './types';
 export const fetchUser = () => async dispatch => {
   const res = await axios.get('/api/current_user');
 
-  dispatch({ type: FETCH_USER, payload: res });
+  dispatch({ type: FETCH_USER, payload: res.data });
 };
--- server/client/src/reducers/authReducer.js
+++ server/client/src/reducers/authReducer.js
@@ -1,6 +1,9 @@
-export default function(state = {}, action) {
-  console.log(action);
+import { FETCH_USER } from '../actions/types';
+
+export default function(state = null, action) {
   switch (action.type) {
+    case FETCH_USER:
+      return action.payload || false;
     default:
       return state;
   }
