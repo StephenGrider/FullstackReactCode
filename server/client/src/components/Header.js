088705b93c3d9fc1c4b441aac593676597fd1c1a
stephengrider <ste.grider@gmail.com>
2017-07-11 11:28:44 -0700

    access state

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return 'Still deciding';
+      case false:
+        return 'im loggedout';
+      default:
+        return 'im logged in';
+    }
+  }
+
   render() {
     return (
       <nav>
@@ -9,9 +21,7 @@ class Header extends Component {
             Emaily
           </a>
           <ul className="right">
-            <li>
-              <a>Login With Google</a>
-            </li>
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
@@ -19,4 +29,8 @@ class Header extends Component {
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
