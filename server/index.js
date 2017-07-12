22d51ee34dffe15723ef7745426a4faa35e0d862
stephengrider <ste.grider@gmail.com>
2017-07-12 16:24:43 -0700

    post handler

--- server/index.js
+++ server/index.js
@@ -20,6 +20,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
