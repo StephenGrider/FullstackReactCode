f01b77c461745c9d5a72e28d08ffba2ea8b82e73
stephengrider <ste.grider@gmail.com>
2017-07-12 16:49:30 -0700

    create charge object

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -2,5 +2,12 @@ const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
 
 module.exports = app => {
-  app.post('/api/stripe', (req, res) => {});
+  app.post('/api/stripe', (req, res) => {
+    stripe.charges.create({
+      amount: 500,
+      currency: 'usd',
+      description: '$5 for 5 credits',
+      source: req.body.id
+    });
+  });
 };
