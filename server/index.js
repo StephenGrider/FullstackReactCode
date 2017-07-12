df40000ebfaad22957c0142e4a339fdd7890c40c
stephengrider <ste.grider@gmail.com>
2017-07-12 16:39:00 -0700

    added boyparser

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
@@ -10,6 +11,7 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -1,3 +1,6 @@
+const keys = require('../config/keys');
+const stripe = require('stripe')(keys.stripeSecretKey);
+
 module.exports = app => {
   app.post('/api/stripe', (req, res) => {});
 };
