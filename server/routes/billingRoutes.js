c14d1b93ebeb33893b8a87049761307a0d0d3127
stephengrider <ste.grider@gmail.com>
2017-07-12 16:55:51 -0700

    finalize charge

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -2,12 +2,14 @@ const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
 
 module.exports = app => {
-  app.post('/api/stripe', (req, res) => {
-    stripe.charges.create({
+  app.post('/api/stripe', async (req, res) => {
+    const charge = await stripe.charges.create({
       amount: 500,
       currency: 'usd',
       description: '$5 for 5 credits',
       source: req.body.id
     });
+
+    console.log(charge);
   });
 };
