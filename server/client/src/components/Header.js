b013addbe566f346f7ac3aaa01e851b21b46cf68
stephengrider <ste.grider@gmail.com>
2017-07-12 14:25:01 -0700

    fix header

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -12,8 +12,8 @@ class Header extends Component {
         return <li><a href="/auth/google">Login With Google</a></li>;
       default:
         return [
-          <li><Payments /></li>,
-          <li><a href="/api/logout">Logout</a></li>
+          <li key="1"><Payments /></li>,
+          <li key="2"><a href="/api/logout">Logout</a></li>
         ];
     }
   }
--- server/client/src/components/Payments.js
+++ server/client/src/components/Payments.js
@@ -5,10 +5,16 @@ class Payments extends Component {
   render() {
     return (
       <StripeCheckout
+        name="Emaily"
+        description="$5 for 5 email credits"
         amount={500}
         token={token => console.log(token)}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
-      />
+      >
+        <button className="btn">
+          Add Credits
+        </button>
+      </StripeCheckout>
     );
   }
 }
