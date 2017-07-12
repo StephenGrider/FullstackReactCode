94bb7b585e107d43932def74b56b30396a5630f6
stephengrider <ste.grider@gmail.com>
2017-07-12 14:39:37 -0700

    reuse action type

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -6,3 +6,9 @@ export const fetchUser = () => async dispatch => {
 
   dispatch({ type: FETCH_USER, payload: res.data });
 };
+
+export const handleToken = token => async dispatch => {
+  const res = await axios.post('/api/stripe', token);
+
+  dispatch({ type: FETCH_USER, payload: res.data });
+};
