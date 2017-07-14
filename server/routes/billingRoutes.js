d599abda714ad44560fe83ef80c545287162a970
stephengrider <ste.grider@gmail.com>
2017-07-14 11:16:53 -0700

    check login

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -3,6 +3,10 @@ const stripe = require('stripe')(keys.stripeSecretKey);
 
 module.exports = app => {
   app.post('/api/stripe', async (req, res) => {
+    if (!req.user) {
+      return res.status(401).send({ error: 'You must log in!' });
+    }
+
     const charge = await stripe.charges.create({
       amount: 500,
       currency: 'usd',
