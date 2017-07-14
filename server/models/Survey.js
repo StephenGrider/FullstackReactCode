5b5e48acfa469c2d1393c69cc9dd7b36d7ed557c
stephengrider <ste.grider@gmail.com>
2017-07-14 16:06:26 -0700

    add model prop

--- server/models/Survey.js
+++ server/models/Survey.js
@@ -5,7 +5,9 @@ const surveySchema = new Schema({
   title: String,
   body: String,
   subject: String,
-  recipients: [String]
+  recipients: [String],
+  yes: { type: Number, default: 0 },
+  no: { type: Number, default: 0 }
 });
 
 mongoose.model('surveys', surveySchema);
