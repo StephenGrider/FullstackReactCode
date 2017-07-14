a8ec425e714272ad8c2e034f69696abe2651eb9a
stephengrider <ste.grider@gmail.com>
2017-07-14 16:49:39 -0700

    finish survey schema

--- server/models/Survey.js
+++ server/models/Survey.js
@@ -8,7 +8,10 @@ const surveySchema = new Schema({
   subject: String,
   recipients: [RecipientSchema],
   yes: { type: Number, default: 0 },
-  no: { type: Number, default: 0 }
+  no: { type: Number, default: 0 },
+  _user: { type: Schema.Types.ObjectId, ref: 'User' },
+  dateSent: Date,
+  lastResponded: Date
 });
 
 mongoose.model('surveys', surveySchema);
