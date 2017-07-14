0c8680f01f67ea9a205409e0c2b15c7120108249
stephengrider <ste.grider@gmail.com>
2017-07-14 11:35:02 -0700

    quantity of credits

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -13,6 +13,9 @@ class Header extends Component {
       default:
         return [
           <li key="1"><Payments /></li>,
+          <li key="3" style={{ margin: '0 10px' }}>
+            Credits: {this.props.auth.credits}
+          </li>,
           <li key="2"><a href="/api/logout">Logout</a></li>
         ];
     }
