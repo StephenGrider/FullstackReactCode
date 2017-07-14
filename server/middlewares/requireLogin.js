8f6aee4a76f1aea858301de605348d22319c651b
stephengrider <ste.grider@gmail.com>
2017-07-14 11:30:01 -0700

    route specific middleware

---
+++ server/middlewares/requireLogin.js
@@ -0,0 +1,7 @@
+module.exports = (req, res, next) => {
+  if (!req.user) {
+    return res.status(401).send({ error: 'You must log in!' });
+  }
+
+  next();
+};
--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -1,12 +1,9 @@
 const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
+const requireLogin = require('../middlewares/requireLogin');
 
 module.exports = app => {
-  app.post('/api/stripe', async (req, res) => {
-    if (!req.user) {
-      return res.status(401).send({ error: 'You must log in!' });
-    }
-
+  app.post('/api/stripe', requireLogin, async (req, res) => {
     const charge = await stripe.charges.create({
       amount: 500,
       currency: 'usd',
