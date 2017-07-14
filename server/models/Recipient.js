960a15c3c123c0f1e9fcd06eacd8c6e915a45034
stephengrider <ste.grider@gmail.com>
2017-07-14 16:43:43 -0700

    add subdoc

---
+++ server/models/Recipient.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const recipientSchema = new Schema({
+  email: String,
+  responded: { type: Boolean, default: false }
+});
+
+module.exports = recipientSchema;
--- server/models/Survey.js
+++ server/models/Survey.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+const RecipientSchema = require('./Recipient');
 
 const surveySchema = new Schema({
   title: String,
   body: String,
   subject: String,
-  recipients: [String],
+  recipients: [RecipientSchema],
   yes: { type: Number, default: 0 },
   no: { type: Number, default: 0 }
 });
