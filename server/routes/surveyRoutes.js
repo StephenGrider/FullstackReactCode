f345c1dbe54f623d611db51eb8fa487928024e09
stephengrider <ste.grider@gmail.com>
2017-07-17 15:28:53 -0700

    mailer in use

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 const Survey = mongoose.model('surveys');
 
@@ -18,6 +19,7 @@ module.exports = app => {
       dateSent: Date.now()
     });
 
-    const mailer = new Mailer(survey, template(survey));
+    // Great place to send an email!
+    const mailer = new Mailer(survey, surveyTemplate(survey));
   });
 };
---
+++ server/services/emailTemplates/surveyTemplate.js
@@ -0,0 +1,3 @@
+module.exports = survey => {
+  return '<div>' + survey.body + '</div>';
+};
