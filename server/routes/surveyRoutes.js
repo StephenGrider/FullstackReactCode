e4880715527f01617568c7ce3d0cefd3e4afe7be
stephengrider <ste.grider@gmail.com>
2017-07-17 11:27:40 -0700

    create survey

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -1,6 +1,17 @@
+const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 
+const Survey = mongoose.model('surveys');
+
 module.exports = app => {
-  app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {});
+  app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+    const { title, subject, body, recipients } = req.body;
+
+    const survey = new Survey({
+      title,
+      subject,
+      body
+    });
+  });
 };
