065f60caac0a4c8e1c47cfe58fedcc0d7a6c51fc
stephengrider <ste.grider@gmail.com>
2017-07-17 14:13:23 -0700

    sendgrid

--- server/config/prod.js
+++ server/config/prod.js
@@ -5,5 +5,6 @@ module.exports = {
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
   stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-  stripeSecretKey: process.env.STRIPE_SECRET_KEY
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+  sendGridKey: process.env.SEND_GRID_KEY
 };
