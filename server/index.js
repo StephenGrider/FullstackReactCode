454eeba56e3cf6fd24240b7f2fb5cc74cf48a410
stephengrider <ste.grider@gmail.com>
2017-07-17 16:49:44 -0700

    improve template

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ require('./models/User');
 require('./models/Survey');
 require('./services/passport');
 
+mongoose.Promise = global.Promise;
 mongoose.connect(keys.mongoURI);
 
 const app = express();
