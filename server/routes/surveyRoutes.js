1f1d49a56de7f1627322d4c89ca782fa7bc285b8
stephengrider <ste.grider@gmail.com>
2017-07-20 15:37:20 -0700

    parse route

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -1,3 +1,6 @@
+const _ = require('lodash');
+const Path = require('path-parser');
+const { URL } = require('url');
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
@@ -12,8 +15,11 @@ module.exports = app => {
   });
 
   app.post('/api/surveys/webhooks', (req, res) => {
-    console.log(req.body);
-    res.send({});
+    const events = _.map(req.body, event => {
+      const pathname = new URL(event.url).pathname;
+      const p = new Path('/api/surveys/:surveyId/:choice');
+      console.log(p.test(pathname));
+    });
   });
 
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
