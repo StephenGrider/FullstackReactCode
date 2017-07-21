b57350083621ccdbe7900b316604616b592b128f
stephengrider <ste.grider@gmail.com>
2017-07-21 14:40:10 -0700

    wiring surveys

--- server/client/src/reducers/index.js
+++ server/client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { reducer as reduxForm } from 'redux-form';
 import authReducer from './authReducer';
+import surveysReducer from './surveysReducer';
 
 export default combineReducers({
   auth: authReducer,
-  form: reduxForm
+  form: reduxForm,
+  surveys: surveysReducer
 });
---
+++ server/client/src/reducers/surveysReducer.js
@@ -0,0 +1,10 @@
+import { FETCH_SURVEYS } from '../actions/types';
+
+export default function(state = [], action) {
+  switch (action.type) {
+    case FETCH_SURVEYS:
+      return action.payload;
+    default:
+      return state;
+  }
+}
