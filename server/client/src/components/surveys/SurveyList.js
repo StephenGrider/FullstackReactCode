9e49251ef63aac465f0b00f82113b8c0a9da6d43
stephengrider <ste.grider@gmail.com>
2017-07-21 14:48:18 -0700

    wire react to redux

---
+++ server/client/src/components/surveys/SurveyList.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchSurveys } from '../../actions';
+
+class SurveyList extends Component {
+  componentDidMount() {
+    this.props.fetchSurveys();
+  }
+
+  render() {
+    return (
+      <div>
+        SurveyList
+      </div>
+    );
+  }
+}
+
+function mapStateToProps({ surveys }) {
+  return { surveys };
+}
+
+export default connect(mapStateToProps, { fetchSurveys })(SurveyList);
