8d1a0ea07676ba3084b7ff187e10e114a586a009
stephengrider <ste.grider@gmail.com>
2017-07-21 15:00:44 -0700

    survey-list

--- server/client/src/components/surveys/SurveyList.js
+++ server/client/src/components/surveys/SurveyList.js
@@ -7,10 +7,32 @@ class SurveyList extends Component {
     this.props.fetchSurveys();
   }
 
+  renderSurveys() {
+    return this.props.surveys.map(survey => {
+      return (
+        <div className="card darken-1" key={survey._id}>
+          <div className="card-content">
+            <span className="card-title">{survey.title}</span>
+            <p>
+              {survey.body}
+            </p>
+            <p className="right">
+              Sent On: {new Date(survey.dateSent).toLocaleDateString()}
+            </p>
+          </div>
+          <div className="card-action">
+            <a>Yes: {survey.yes}</a>
+            <a>No: {survey.no}</a>
+          </div>
+        </div>
+      );
+    });
+  }
+
   render() {
     return (
       <div>
-        SurveyList
+        {this.renderSurveys()}
       </div>
     );
   }
