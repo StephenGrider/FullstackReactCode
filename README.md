03784dff299f156f89549fbee883dca06dfa6d66
ontoneio <ontoneio.mm@gmail.com>
2018-02-18 14:07:05 -0800

    Removes diagrams/ from deployment

--- README.md
+++
@@ -1,3 +0,0 @@
-# FullstackReactCode
-
-Companion repo to a course on Udemy.com. See here: https://www.udemy.com/node-with-react-fullstack-web-development
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+// TONE-Mail
+
 app.get('/', (req, res) => {
   res.send({hi: 'there'});
 })
