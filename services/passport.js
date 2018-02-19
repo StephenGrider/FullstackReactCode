8785c947bc16e76190c1baa36eb2b80448a7f985
ontoneio <ontoneio.mm@gmail.com>
2018-02-19 00:24:50 -0800

    Tweak google strategy settings

--- services/passport.js
+++ services/passport.js
@@ -20,7 +20,8 @@ passport.use(
   new GoogleStrategy({
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
+      callbackURL: '/auth/google/callback',
+      proxy: true
     }, 
     (accessToken, refreshToken, profile, done) => {
       User.findOne({googleID: profile.id}).then((existingUser) => {
