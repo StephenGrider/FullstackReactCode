aa1388d1d20684b0e5e6fd985ebe93eb9d3fe6ec
ontoneio <ontoneio.mm@gmail.com>
2018-03-08 14:14:46 -0800

    Import and setup Link component for auth flow parent Header component

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
   renderContent() {
@@ -11,7 +12,7 @@ class Header extends Component {
           <li><a href="/auth/google">Login With Google</a></li>
         );
       default:
-        return <li><a href="">Logout</a></li>
+        return <li><a href="/api/logout">Logout</a></li>
     }
   }
 
@@ -21,9 +22,12 @@ class Header extends Component {
     return (
       <nav>
         <div className="nav-wrapper">
-          <a className="left brand-logo">
+          <Link 
+          to={this.props.auth ? '/surveys' : '/'}          
+          className="left brand-logo"
+          >
             Tone-iO
-          </a>
+          </Link>
           <ul className="right">
             {this.renderContent()}
           </ul>
