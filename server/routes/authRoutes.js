78df5313374d9cd3d155dfe438502e0a197afdc7
ontoneio <ontoneio.mm@gmail.com>
2018-03-08 14:16:10 -0800

    Redirect /api/logout route to root route '/'

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -19,7 +19,7 @@ module.exports = app => {
 
   app.get('/api/logout', (req, res) => {
     req.logout();
-    res.send(req.user);
+    res.redirect('/')
   })
 
   app.get('/api/current_user', (req, res) => {
