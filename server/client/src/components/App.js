5303887edec92dc1354c50419433a638f5a4564b
ontoneio <ontoneio.mm@gmail.com>
2018-03-08 09:40:43 -0800

    Update client side App components for visualizing login process

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
-const App = () => {
-  return (
-    <div>
-      On-tone-iO
-    </div>
-  )
-}
+import Header from './Header';
+const Dashboard = () => <h2>Dashboard</h2>
+const SurveyNew = () => <h2>SurveyNew</h2>
+const Landing = () => <h2>Landing</h2>
 
-export default App;
+class App extends Component {
+
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />           
+            <Route path="/surveys/new" component={SurveyNew} />           
+          </div>
+        </BrowserRouter>
+      </div>
+    )
+  }
+} 
+
+
+export default connect(null, actions)(App);
--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,12 +1,15 @@
+import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
+import reducers from './components/reducers';
 
-const store = createStore(() => [], {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
-  < Provider store={store}> <App /> </Provider>,  
+  <Provider store={store}><App /></Provider>,  
   document.querySelector('#root'));
