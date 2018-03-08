d7551920b351801cede6c041a13200e3434c475f
ontoneio <ontoneio.mm@gmail.com>
2018-03-08 14:17:11 -0800

    Landing page component

---
+++ server/client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div style={{textAlign: 'center'}}>
+      <h1>
+        ~Tone-iO~
+      </h1>
+      Collect feedback from your users
+    </div>
+  );
+};
+
+export default Landing;
