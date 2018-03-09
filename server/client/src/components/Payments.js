caf9f8fc63a3f4a2c7a517d8a6c255f7626a7726
ontoneio <ontoneio.mm@gmail.com>
2018-03-08 19:08:34 -0800

    Add middlware requireLogin.js, add route billingRoutes.js, add component Payment.js

---
+++ server/client/src/components/Payments.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
+class Payments extends Component {
+  render() {
+    return (
+      <StripeCheckout
+        name="Tone-iO"
+        description="$5 for 5 email credits" 
+        amount={500}
+        token={token => this.props.handleToken(token)}
+        stripeKey={process.env.REACT_APP_STRIPE_KEY}
+      >
+        <button className="btn" >
+          Add Credits
+        </button>
+      </StripeCheckout>
+    );
+  }
+}
+
+export default connect(null, actions)(Payments);
