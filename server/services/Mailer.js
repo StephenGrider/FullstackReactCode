51b9ae7e99bf8ea267e458efac536ebdd148667c
stephengrider <ste.grider@gmail.com>
2019-07-17 15:41:42 -0700

    mailer

--- server/services/Mailer.js
+++ server/services/Mailer.js
@@ -2,6 +2,15 @@ const sendgrid = require('sendgrid');
 const helper = sendgrid.mail;
 const keys = require('../config/keys');
 
-class Mailer extends helper.Mail {}
+class Mailer extends helper.Mail {
+  constructor({ subject, recipients }, content) {
+    super();
+
+    this.from_email = new helper.Email('no-reply@emaily.com');
+    this.subject = subject;
+    this.body = new helper.Content('text/html', content);
+    this.recipients = this.formatAddresses(recipients);
+  }
+}
 
 module.exports = Mailer;
