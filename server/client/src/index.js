68d0cfad47a9e738fb9dc7b742ca4d699c3a0ec9
stephengrider <ste.grider@gmail.com>
2019-07-17 16:49:44 -0700

    improve template

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -8,6 +8,10 @@ import reduxThunk from 'redux-thunk';
 import App from './components/App';
 import reducers from './reducers';
 
+// Development only axios helpers!
+import axios from 'axios';
+window.axios = axios;
+
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
--- server/services/emailTemplates/surveyTemplate.js
+++ server/services/emailTemplates/surveyTemplate.js
@@ -1,3 +1,19 @@
 module.exports = survey => {
-  return '<div>' + survey.body + '</div>';
+  return `
+    <html>
+      <body>
+        <div style="text-align: center;">
+          <h3>I'd like your input!</h3>
+          <p>Please answer the following question:</p>
+          <p>${survey.body}</p>
+          <div>
+            <a href="http://localhost:3000">Yes</a>
+          </div>
+          <div>
+            <a href="http://localhost:3000">No</a>
+          </div>
+        </div>
+      </body>
+    </html>
+  `;
 };
