3728ecf99aa3f7ea1e726471aa4a16605a1b948f
stephengrider <ste.grider@gmail.com>
2019-07-17 17:38:24 -0700

    feedback for feedback

--- server/services/emailTemplates/surveyTemplate.js
+++ server/services/emailTemplates/surveyTemplate.js
@@ -1,3 +1,5 @@
+const keys = require('../../config/keys');
+
 module.exports = survey => {
   return `
     <html>
@@ -7,10 +9,10 @@ module.exports = survey => {
           <p>Please answer the following question:</p>
           <p>${survey.body}</p>
           <div>
-            <a href="http://localhost:3000">Yes</a>
+            <a href="${keys.redirectDomain}/api/surveys/thanks">Yes</a>
           </div>
           <div>
-            <a href="http://localhost:3000">No</a>
+            <a href="${keys.redirectDomain}/api/surveys/thanks">No</a>
           </div>
         </div>
       </body>
