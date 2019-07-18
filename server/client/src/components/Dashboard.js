5acd3954dbe6c9e534a4ac8e6b39eeb6e5cec0e4
stephengrider <ste.grider@gmail.com>
2019-07-18 13:34:13 -0700

    nav with link

--- server/client/src/components/Dashboard.js
+++ server/client/src/components/Dashboard.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Dashboard = () => {
   return (
     <div>
       Dashboard
       <div className="fixed-action-btn">
-        <a className="btn-floating btn-large red">
+        <Link to="/surveys/new" className="btn-floating btn-large red">
           <i className="material-icons">add</i>
-        </a>
+        </Link>
       </div>
     </div>
   );
