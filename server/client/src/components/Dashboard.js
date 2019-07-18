fabeb31ceda3313b217eef32bd02520c1ea5e0dd
stephengrider <ste.grider@gmail.com>
2019-07-18 13:25:45 -0700

    material icons

--- server/client/src/components/Dashboard.js
+++ server/client/src/components/Dashboard.js
@@ -4,6 +4,11 @@ const Dashboard = () => {
   return (
     <div>
       Dashboard
+      <div className="fixed-action-btn">
+        <a className="btn-floating btn-large red">
+          <i className="material-icons">add</i>
+        </a>
+      </div>
     </div>
   );
 };
