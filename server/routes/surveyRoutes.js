43127646c5ec23dc986e1a6c6c31a567ebab25c1
stephengrider <ste.grider@gmail.com>
2019-07-20 16:30:05 -0700

    lodash chain

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -15,19 +15,20 @@ module.exports = app => {
   });
 
   app.post('/api/surveys/webhooks', (req, res) => {
-    const events = _.map(req.body, ({ email, url }) => {
-      const pathname = new URL(url).pathname;
-      const p = new Path('/api/surveys/:surveyId/:choice');
-      const match = p.test(pathname);
-      if (match) {
-        return { email, surveyId: match.surveyId, choice: match.choice };
-      }
-    });
+    const p = new Path('/api/surveys/:surveyId/:choice');
 
-    const compactEvents = _.compact(events);
-    const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId');
+    const events = _.chain(req.body)
+      .map(({ email, url }) => {
+        const match = p.test(new URL(url).pathname);
+        if (match) {
+          return { email, surveyId: match.surveyId, choice: match.choice };
+        }
+      })
+      .compact()
+      .uniqBy('email', 'surveyId')
+      .value();
 
-    console.log(uniqueEvents);
+    console.log(events);
 
     res.send({});
   });
