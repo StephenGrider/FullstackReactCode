8607eb236b47bb93ae91fe7fc8855c2df4714d1e
stephengrider <ste.grider@gmail.com>
2019-07-20 12:12:52 -0700

    redirect

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -13,8 +13,9 @@ export const handleToken = token => async dispatch => {
   dispatch({ type: FETCH_USER, payload: res.data });
 };
 
-export const submitSurvey = values => async dispatch => {
+export const submitSurvey = (values, history) => async dispatch => {
   const res = await axios.post('/api/surveys', values);
 
+  history.push('/surveys');
   dispatch({ type: FETCH_USER, payload: res.data });
 };
