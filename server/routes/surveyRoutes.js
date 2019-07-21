7e35057a69e8881cb9c76adadfdc8432d777dfe6
stephengrider <ste.grider@gmail.com>
2019-07-21 12:37:38 -0700

    added stuff

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -10,7 +10,7 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-  app.get('/api/surveys/thanks', (req, res) => {
+  app.get('/api/surveys/:surveyId/:choice', (req, res) => {
     res.send('Thanks for voting!');
   });
 
@@ -36,7 +36,8 @@ module.exports = app => {
           },
           {
             $inc: { [choice]: 1 },
-            $set: { 'recipients.$.responded': true }
+            $set: { 'recipients.$.responded': true },
+            lastResponded: new Date()
           }
         ).exec();
       })
