a0b3fd56b34fb25ee570ef3318fc160e467559c8
stephengrider <ste.grider@gmail.com>
2019-07-21 14:40:10 -0700

    wiring surveys

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { FETCH_USER } from './types';
+import { FETCH_USER, FETCH_SURVEYS } from './types';
 
 export const fetchUser = () => async dispatch => {
   const res = await axios.get('/api/current_user');
@@ -18,4 +18,10 @@ export const submitSurvey = (values, history) => async dispatch => {
 
   history.push('/surveys');
   dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+export const fetchSurveys = () => async dispatch => {
+  const res = await axios.get('/api/surveys');
+
+  dispatch({ type: FETCH_SURVEYS, payload: res.data });
 };
