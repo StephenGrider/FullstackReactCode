a358c1213b74db189545bb48a5c11c94b28e58c5
stephengrider <ste.grider@gmail.com>
2019-07-21 11:46:00 -0700

    add query

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -17,7 +17,7 @@ module.exports = app => {
   app.post('/api/surveys/webhooks', (req, res) => {
     const p = new Path('/api/surveys/:surveyId/:choice');
 
-    const events = _.chain(req.body)
+    _.chain(req.body)
       .map(({ email, url }) => {
         const match = p.test(new URL(url).pathname);
         if (match) {
@@ -26,9 +26,20 @@ module.exports = app => {
       })
       .compact()
       .uniqBy('email', 'surveyId')
-      .value();
-
-    console.log(events);
+      .each(({ email, choice, surveyId }) => {
+        Survey.updateOne(
+          {
+            id: surveyId,
+            recipients: {
+              $elemMatch: { email: email, responded: false }
+            }
+          },
+          {
+            $inc: { [choice]: 1 },
+            $set: { 'recipients.$.responded': true }
+          }
+        );
+      });
 
     res.send({});
   });
