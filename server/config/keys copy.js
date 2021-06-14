2e3e27c40b4325c68b4e76ac0f767e0410b855ee
gl3127 <fjabbari@ventera.com>
2021-06-14 08:47:41 -0500

    init5

--- server/config/keys copy.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  googleClientID: '548271082756-krvo7o9n25mpfd71e3k2g7rahlk7olo4.apps.googleusercontent.com',
-  googleClientSecret: 'LNtorf92cZqqVtxAniyo2PLT',
-  mongoURI: 'mongodb+srv://ffjabbari:15pishyy@emaily-dev.x7ckf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-  cookieKey: 'fredman'
-};
